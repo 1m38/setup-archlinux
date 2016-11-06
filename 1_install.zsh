00623068c0ee1af54fd39c73d10b15c9b97ddaa6
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2016-11-06 16:02:02 +0900

    Initial commit

---
+++ 1_install.zsh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+# 参考: http://kunst1080.hatenablog.com/entry/2014/10/19/212305
+
+vared -p "Hostname: " -c NEWHOSTNAME
+vared -p "root passwd: " -c ROOTPASSWD
+
+
+loadkeys jp106
+# setup HDD (4GB swap)
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda -- mkpart primary ext2 0 2MB
+parted -s /dev/sda -- mkpart primary ext4 2MB -4098MB
+parted -s /dev/sda -- mkpart primary ext4 -4097MB -1MB
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# Install Base system
+mount /dev/sda1 /mnt
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+grep jp /etc/pacman.d/mirrorlist.old > /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist.old >> /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel
+
+# fstab
+genfstab -p /mnt >> /mnt/etc/fstab
+# hostname
+echo $NEWHOSTNAME > /mnt/etc/hostname
+
+# Run setup script on chroot environment
+mkdir -p /mnt/root
+cp -f `dirname $0`/chroot-setup.sh /mnt/root/chroot-setup.sh
+echo "echo root:$ROOTPASSWD | chpasswd" >> /mnt/root/chroot-setup.sh
+chmod +x /mnt/root/chroot-setup.sh
+arch-chroot /mnt /root/chroot-setup.sh
